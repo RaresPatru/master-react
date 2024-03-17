c9192615556955c90eb6ac415e7f0877fd33f46b
Rares Patru <rares.patru20@gmail.com>
2024-03-17 16:37:58 -0400

    cleaned boilerplate data from data.js

--- src/components/accordion/data.js
+++ src/components/accordion/data.js
@@ -4,14 +4,12 @@ const data = [
     question: "What are accordion components?",
     answer:
       "Accordion components are user interface elements used for organizing and presenting content in a collapsible manner. They typically consist of a header, content, and an expand/collapse action.",
-    newProp: 5,
   },
   {
     id: "2",
     question: "What are they used for?",
     answer:
       "They are commonly employed in various contexts, including FAQs, product descriptions, navigation menus, settings panels, and data tables, to save screen space and provide a structured and user-friendly interface for presenting information or options.",
-    newProp: "text",
   },
   {
     id: "3",
@@ -24,7 +22,6 @@ const data = [
     question: "Can I create an accordion component with a different framework?",
     answer:
       "Yes of course, it is very possible to create an accordion component with another framework.",
-    newProp: "text",
   },
 ];
 

56b6ef2be320ad6208cbed7fbd0f9077e47bb52e
Rares Patru <rares.patru20@gmail.com>
2024-03-29 16:56:31 -0400

    The image-slider component fetches API data and renders images whenever an arrow or a position circle is clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Accordion from "./components/accordion";
+import ImageSlider from "./components/image-slider";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
@@ -12,6 +13,8 @@ function App() {
       <RandomColor />
       {/* Star rating component */}
       <StarRating numOfStars={10} />
+      {/* Image slider component */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} limit={10} page={3} />
     </div>
   );
 }
---
+++ src/components/image-slider/index.tsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import "./styles.css";
+
+interface ImageSliderUrlProps<T = number> {
+  url: string;
+  limit?: T;
+  page?: T;
+}
+
+export default function ImageSlider({
+  url,
+  limit = 5,
+  page = 1,
+}: ImageSliderUrlProps<number>) {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchImages(getUrl: string) {
+    try {
+      setLoading(true);
+
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
+
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (e: any) {
+      setErrorMsg(e.message);
+      setLoading(false);
+    }
+  }
+
+  function handlePrevious() {
+    setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
+  }
+
+  function handelNext() {
+    setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
+  }
+
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]);
+
+  if (loading) {
+    return <div>Loading data! Please wait</div>;
+  }
+
+  if (errorMsg !== null) {
+    return <div>Error encountered! {errorMsg} </div>;
+  }
+
+  return (
+    <div className="container">
+      <BsArrowLeftCircleFill
+        onClick={handlePrevious}
+        className="arrow arrow-left"
+      />
+      {images && images.length
+        ? images.map((imageItem: any, index) => (
+            <img
+              key={imageItem.id}
+              alt={imageItem.download_url}
+              src={imageItem.download_url}
+              className={
+                currentSlide === index
+                  ? "current-image"
+                  : "current-image hide-current-image"
+              }
+            />
+          ))
+        : null}
+      <BsArrowRightCircleFill
+        onClick={handelNext}
+        className="arrow arrow-right"
+      />
+      <span className="circle-indicators">
+        {images && images.length
+          ? images.map((_, index) => (
+              <button
+                key={index}
+                className={
+                  currentSlide === index
+                    ? "current-indicator"
+                    : "current-indicator idle-indicator"
+                }
+                onClick={() => setCurrentSlide(index)}
+              ></button>
+            ))
+          : null}
+      </span>
+    </div>
+  );
+}

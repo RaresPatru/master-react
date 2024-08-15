c1ee7a04d02f2a1fd84854e6abea9e05b1a66357
Rares Patru <rares.patru20@gmail.com>
2024-08-16 02:12:56 +0300

    Implemented the scroll progress functionality and added styling to showcase the progress

--- src/components/scroll-progress/index.tsx
+++ src/components/scroll-progress/index.tsx
@@ -8,9 +8,10 @@ export default function ScrollProgress({
   url: string;
   limit: number;
 }) {
-  const [data, setData] = useState<string[]>([]);
+  const [data, setData] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<null | string>(null);
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
   async function fetchData(getUrl: string) {
     try {
@@ -33,13 +34,48 @@ export default function ScrollProgress({
     fetchData(url);
   }, [url, limit]);
 
+  function hadleScrollPercentage() {
+    const howMuchScrolled =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    setScrollPercentage((howMuchScrolled / height) * 100);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", hadleScrollPercentage);
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
   if (errorMessage !== null) {
     return <p className="stylish-error">{errorMessage}</p>;
   }
-  console.log(data, loading);
+
+  if (loading) {
+    return <div>Loading data.. Please wait ^_^</div>;
+  }
+
+  console.log(scrollPercentage);
+
   return (
     <div>
-      <h1>Custom Scroll Indicator</h1>
+      <div className="top-container">
+        <h1>Custom Scroll Indicator</h1>
+        <div className="scroll-tracking-container">
+          <div
+            className="progress-bar"
+            style={{ width: `${scrollPercentage}%` }}
+          ></div>
+        </div>
+      </div>
+      <div className="data-container">
+        {data && data.length > 0
+          ? data.map((dataItem) => <p key={dataItem.id}>{dataItem.title}</p>)
+          : null}
+      </div>
     </div>
   );
 }
